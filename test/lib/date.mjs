088bbf2856ce8b87d7a75edc2c006ccdf07e0a14
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2023-10-08 16:12:20 +0200

    Fixes INTERVALS.6h const
    fixes #14

--- test/lib/date.mjs
+++ test/lib/date.mjs
@@ -1,5 +1,5 @@
 import { describe, it } from 'node:test'
-import { ok, strictEqual } from 'node:assert'
+import { ok, equal } from 'node:assert/strict'
 
 import { INTERVALS, parseInterval, daysBetween, jsonDateReviver, utcDate } from '../../lib/date.mjs'
 
@@ -26,54 +26,68 @@ describe('lib/date', () => {
   })
   describe('.daysBetween', () => {
     it('is callable', () => {
-      strictEqual(typeof daysBetween, 'function')
+      equal(typeof daysBetween, 'function')
     })
     it('returns a number', () => {
-      strictEqual(typeof daysBetween(new Date(), new Date()), 'number')
+      equal(typeof daysBetween(new Date(), new Date()), 'number')
     })
     it('returns the number of days between two dates', () => {
-      strictEqual(daysBetween(new Date('2021-09-26'), new Date('2021-09-27')), 1)
+      equal(daysBetween(new Date('2021-09-26'), new Date('2021-09-27')), 1)
     })
     it('returns the number of days between two dates with iso8601 format', () => {
-      strictEqual(daysBetween(new Date('2021-09-26T10:30:00.000Z'), new Date('2021-09-27T10:30:00.000Z')), 1)
+      equal(daysBetween(new Date('2021-09-26T10:30:00.000Z'), new Date('2021-09-27T10:30:00.000Z')), 1)
     })
   })
-  describe.todo('.parseInterval', () => {
+  describe('.parseInterval', () => {
     it('is callable', () => {
-      strictEqual(typeof parseInterval, 'function')
+      equal(typeof parseInterval, 'function')
     })
     it('returns a number', () => {
-      strictEqual(typeof parseInterval('1y'), 'number')
+      equal(typeof parseInterval('1y'), 'number')
     })
     it('handles years|y, months|m, weeks|w, days|d, hours|h, and minutes (without any label)', () => {
-      strictEqual(parseInterval('1y'), 31536000000)
-      strictEqual(parseInterval('1m'), 2592000000)
-      strictEqual(parseInterval('1w'), 604800000)
-      strictEqual(parseInterval('1d'), 86400000)
-      strictEqual(parseInterval('1h'), 3600000)
-      strictEqual(parseInterval('1'), 60000)
+      equal(parseInterval('1y'), INTERVALS.get('1y'))
+      equal(parseInterval('1m'), INTERVALS.get('1m'))
+      equal(parseInterval('1w'), INTERVALS.get('1w'))
+      equal(parseInterval('1d'), INTERVALS.get('1d'))
+      equal(parseInterval('1h'), INTERVALS.get('1h'))
+      equal(parseInterval('1'), INTERVALS.get('1'))
     })
-    it('handles multiples of any given interval', () => {
-      strictEqual(parseInterval('2y'), 63072000000)
-      strictEqual(parseInterval('3m'), 7776000000)
-      strictEqual(parseInterval('2w'), 1209600000)
-      strictEqual(parseInterval('3d'), 259200000)
-      strictEqual(parseInterval('2h'), 7200000)
-      strictEqual(parseInterval('2'), 120000)
+    it('also handles multiples of those intervals', () => {
+      equal(parseInterval('5y'), INTERVALS.get('5y'))
+      equal(parseInterval('3y'), INTERVALS.get('3y'))
+      equal(parseInterval('2y'), INTERVALS.get('2y'))
+      equal(parseInterval('4m'), INTERVALS.get('4m'))
+      equal(parseInterval('3m'), INTERVALS.get('3m'))
+      equal(parseInterval('2m'), INTERVALS.get('2m'))
+      equal(parseInterval('3w'), INTERVALS.get('3w'))
+      equal(parseInterval('2w'), INTERVALS.get('2w'))
+      equal(parseInterval('3d'), INTERVALS.get('3d'))
+      equal(parseInterval('2d'), INTERVALS.get('2d'))
+      equal(parseInterval('12h'), INTERVALS.get('12h'))
+      equal(parseInterval('6h'), INTERVALS.get('6h'))
+      equal(parseInterval('3h'), INTERVALS.get('3h'))
+      equal(parseInterval('2h'), INTERVALS.get('2h'))
+      equal(parseInterval('30'), INTERVALS.get('30'))
+      equal(parseInterval('15'), INTERVALS.get('15'))
+      equal(parseInterval('10'), INTERVALS.get('10'))
+      equal(parseInterval('5'), INTERVALS.get('5'))
+      equal(parseInterval('3'), INTERVALS.get('3'))
+      equal(parseInterval('2'), INTERVALS.get('2'))
     })
   })
   describe('.jsonDateReviver', () => {
     it('is callable', () => {
-      strictEqual(typeof jsonDateReviver, 'function')
+      equal(typeof jsonDateReviver, 'function')
     })
     it('returns a date when given a string', () => {
       ok(jsonDateReviver(null, '2023-09-26T10:30:00.000Z') instanceof Date)
     })
     it('returns the value when given a non-string', () => {
-      strictEqual(jsonDateReviver(null, 42), 42) // Non-string input
+      equal(jsonDateReviver(null, 42), 42) // Non-string input
     })
     it('returns the value when given a string that does not match the iso8601 regex', () => {
-      strictEqual(jsonDateReviver(null, '2023/09/26 10:30:00'), '2023/09/26 10:30:00') // Non-matching format
+      equal(jsonDateReviver(null, '2023/09/26 10:30:00'), '2023/09/26 10:30:00') // Non-matching format
     })
     it('returns a date when given a string that matches the iso8601 regex', () => {
       ok(jsonDateReviver(null, '2023-09-26T10:30:00.000Z') instanceof Date)
@@ -84,14 +98,14 @@ describe('lib/date', () => {
   })
   describe('.utcDate', () => {
     it('is callable', () => {
-      strictEqual(typeof utcDate, 'function')
+      equal(typeof utcDate, 'function')
     })
     it('returns a Date', () => {
       ok(utcDate() instanceof Date)
     })
     it('returns a Date that is adjusted to .getTimezoneOffset ', () => {
       const now = new Date()
-      strictEqual(utcDate(now).toJSON(), new Date(now.getTime() + (now.getTimezoneOffset() * 6e4)).toJSON())
+      equal(utcDate(now).toJSON(), new Date(now.getTime() + (now.getTimezoneOffset() * 6e4)).toJSON())
     })
   })
 })
