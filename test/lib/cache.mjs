31aab58dd08608b6f926d20c79807691dd7491d9
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2023-10-08 16:16:46 +0200

    adds cache.byInterval

--- test/lib/cache.mjs
+++ test/lib/cache.mjs
@@ -1,6 +1,6 @@
 import { describe, it } from 'node:test'
 import { equal } from 'assert/strict'
-import { byExchange, readCache, readLastCachedLineOf, readLastCachedJsonLineOf} from '../../lib/cache.mjs'
+import { byExchange, byInterval, readCache, readLastCachedLineOf, readLastCachedJsonLineOf } from '../../lib/cache.mjs'
 
 describe('lib/cache', () => {
   describe('.byExchange', () => {
@@ -8,8 +8,17 @@ describe('lib/cache', () => {
       equal(typeof byExchange, 'function')
     })
     it('returns a predicate matching the provided exchange name', () => {
-      const matchesIfStartsWithFooComma = byExchange('foo')
-      equal(matchesIfStartsWithFooComma('foo,bar'), true)
+      const matchesIfContainsExchange = byExchange('bybit')
+      equal(matchesIfContainsExchange('bybit,btc-usd,1d.jsonl'), true)
+    })
+  })
+  describe('.byInterval', () => {
+    it('returns a function', () => {
+      equal(typeof byInterval, 'function')
+    })
+    it('returns a predicate matching the provided interval name', () => {
+      const matchesIfContainsInterval = byInterval('1d')
+      equal(matchesIfContainsInterval('coinbase,btc-usd,1d.jsonl'), true)
     })
   })
   describe('.readCache', () => {
