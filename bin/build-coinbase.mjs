d2ed567ea971d944cd90cfd9266561f4fad003a1
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2023-10-13 19:24:55 +0200

    removes build-coinbase-latest
    but also does:
    - fixes convertJsonl
    - moves dateReplacerFor for later testing

--- bin/build-coinbase.mjs
+++ bin/build-coinbase.mjs
@@ -4,6 +4,7 @@ import { createReadStream, createWriteStream } from 'node:fs'
 import { mkdir } from 'node:fs/promises'
 import { readCache, byExchange } from '../lib/cache.mjs'
 import { writeJson, writeCsv, parseJsonl } from '../lib/jsonl.mjs'
+import { dateReplacerFor } from '../lib/date.mjs'
 
 for (const file of await readCache(byExchange('coinbase'))) {
   const [,, id, interval] = file.split(/[/,.]/)
@@ -15,17 +16,11 @@ for (const file of await readCache(byExchange('coinbase'))) {
   await convertJsonl(writeJson, source, `www/api/${id}/${interval}.json`, dateReplacerFor(interval))
 }
 
-function dateReplacerFor (interval) { // todo: would kinda make sense moving this to lib/date.mjs
-  return /\d+[dwmy]/.test(interval)
-    ? (_, value) => value === new Date(value).toJSON() ? new Date(value).toJSON().substring(0, 10) : value
-    : (_, value) => value
-}
-
-function convertJsonl (writeFn, src, dst, timerLabel, replacer) { // todo: this could be maybe better placed in lib/jsonl.mjs or lib/cache.mjs
-  console.time(`bin/build-coinbase @async ${writeFn.name}: ${dst} time`)
+function convertJsonl (write, src, dst, replacer) { // todo: this could be maybe better placed in lib/jsonl.mjs or lib/cache.mjs
+  console.time(`bin/build-coinbase @async ${write.name}: ${dst} time`)
   return src
     .pipe(parseJsonl({ objectMode: true }))
-    .pipe(writeJson({ objectMode: true, replacer }))
+    .pipe(write({ objectMode: true, replacer }))
     .pipe(createWriteStream(dst))
-    .on('close', console.timeEnd.bind(console, `bin/build-coinbase @async ${writeFn.name}: ${dst} time`))
+    .on('close', console.timeEnd.bind(console, `bin/build-coinbase @async ${write.name}: ${dst} time`))
 }
