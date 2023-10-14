1845dfa8c71e0df48743d7ec2ca175f9199f3770
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2023-10-14 19:28:02 +0200

    refactoring to use common function

--- bin/sync-coinbase-latest.mjs
+++ bin/sync-coinbase-latest.mjs
@@ -1,71 +1,21 @@
 #!/usr/bin/env node --experimental-modules
-import { Readable } from 'node:stream'
+
 import { createWriteStream } from 'node:fs'
-import { EOL } from 'node:os'
 import { fetchCoinbase } from '../lib/coinbase.mjs'
+import { writeCsv, writeJson } from '../lib/jsonl.mjs'
 
 const { products } = await fetchCoinbase('brokerage/products')
 
-/* eslint-disable camelcase */
-const latestCsv = new Readable()
-const latestJson = new Readable()
-const count = 0
+const latestCsvWriter = writeCsv(['id', 'base id', 'base name', 'quote id', 'quote name', 'status', 'price', 'volume 24h', 'price % 24h', 'volume % 24h', 'api'])
+const latestJsonWriter = writeJson()
 
-latestCsv.push(['id', 'base id', 'base name', 'quote id', 'quote name', 'status', 'price', 'volume 24h', 'price % 24h', 'volume % 24h', 'api'].join(',') + EOL)
-latestJson.push('[')
-
-for (const product of products) {
-  const { product_id, base_currency_id, base_name, quote_currency_id, quote_name, status, price, volume_24h, price_percentage_change_24h, volume_percentage_change_24h } = product
-  const jsonObject = {
-    id: product_id,
-    baseId: base_currency_id,
-    baseName: base_name,
-    quoteId: quote_currency_id,
-    quoteName: quote_name,
-    status,
-    price,
-    volume24h: volume_24h,
-    pricePercentageChange24h: price_percentage_change_24h,
-    volumePercentageChange24h: volume_percentage_change_24h,
-    api: !['USDC', 'USDT', 'GBP'].includes(quote_currency_id)
-  }
-
-  if (count === 0) {
-    latestJson.push(JSON.stringify(jsonObject))
-  } else {
-    latestJson.push(`${JSON.stringify(jsonObject)},`)
-  }
-
-  latestCsv.push(`${
-    product_id.toLowerCase()
-  },${
-    base_currency_id
-  },${
-    base_name
-  },${
-    quote_currency_id
-  },${
-    quote_name
-  },${
-    status
-  },${
-    price
-  },${
-    volume_24h
-  },${
-    price_percentage_change_24h
-  },${
-    volume_percentage_change_24h
-  },${
-    ['USDC', 'USDT', 'GBP'].includes(quote_currency_id) ? 'FALSE' : 'TRUE'
-  }${
-    EOL
-  }`)
-  /* eslint-enable camelcase */
+for (const { product_id: id, base_currency_id: baseId, base_name: baseName, quote_currency_id: quoteId, quote_name: quoteName, status, price, volume_24h: volume24h, price_percentage_change_24h: pricePercentageChange24h, volume_percentage_change_24h: volumePercentageChange24h } of products) {
+  latestJsonWriter.write({ id: id.toLowerCase(), baseId, baseName, quoteId, quoteName, status, price, volume24h, pricePercentageChange24h, volumePercentageChange24h, api: !['USDC', 'USDT', 'GBP'].includes(quoteId) })
+  latestCsvWriter.write([id.toLowerCase(), baseId, baseName, quoteId, quoteName, status, price, volume24h, pricePercentageChange24h, volumePercentageChange24h, !['USDC', 'USDT', 'GBP'].includes(quoteId)])
 }
-latestJson.push(']')
-latestJson.push(null)
-latestJson.pipe(createWriteStream('www/api/latest.json'))
 
-latestCsv.push(null)
-latestCsv.pipe(createWriteStream('www/api/latest.csv'))
+latestJsonWriter.end()
+latestCsvWriter.end()
+
+latestCsvWriter.pipe(createWriteStream('www/api/latest.csv'))
+latestJsonWriter.pipe(createWriteStream('www/api/latest.json'))
