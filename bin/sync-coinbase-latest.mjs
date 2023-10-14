c131479d323a219d791d1113c285d4bd7ecbd30d
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2023-10-15 01:44:08 +0200

    changes field name from api to cached

--- bin/sync-coinbase-latest.mjs
+++ bin/sync-coinbase-latest.mjs
@@ -6,11 +6,11 @@ import { writeCsv, writeJson } from '../lib/jsonl.mjs'
 
 const { products } = await fetchCoinbase('brokerage/products')
 
-const latestCsvWriter = writeCsv(['id', 'base id', 'base name', 'quote id', 'quote name', 'status', 'price', 'volume 24h', 'price % 24h', 'volume % 24h', 'api'])
+const latestCsvWriter = writeCsv(['id', 'base id', 'base name', 'quote id', 'quote name', 'status', 'price', 'volume 24h', 'price % 24h', 'volume % 24h', 'cached'])
 const latestJsonWriter = writeJson()
 
 for (const { product_id: id, base_currency_id: baseId, base_name: baseName, quote_currency_id: quoteId, quote_name: quoteName, status, price, volume_24h: volume24h, price_percentage_change_24h: pricePercentageChange24h, volume_percentage_change_24h: volumePercentageChange24h } of products) {
-  latestJsonWriter.write({ id: id.toLowerCase(), baseId, baseName, quoteId, quoteName, status, price, volume24h, pricePercentageChange24h, volumePercentageChange24h, api: !['USDC', 'USDT', 'GBP'].includes(quoteId) })
+  latestJsonWriter.write({ id: id.toLowerCase(), baseId, baseName, quoteId, quoteName, status, price, volume24h, pricePercentageChange24h, volumePercentageChange24h, cached: !['USDC', 'USDT', 'GBP'].includes(quoteId) })
   latestCsvWriter.write([id.toLowerCase(), baseId, baseName, quoteId, quoteName, status, price, volume24h, pricePercentageChange24h, volumePercentageChange24h, !['USDC', 'USDT', 'GBP'].includes(quoteId)])
 }
 
