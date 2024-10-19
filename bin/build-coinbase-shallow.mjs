dff8ed3efad26ef62efbb0ef957b9157d8eabc95
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2024-10-20 01:19:07 +0200

    improves logging and fixes typo in file-path

--- bin/build-coinbase-shallow.mjs
+++ bin/build-coinbase-shallow.mjs
@@ -2,31 +2,23 @@
 
 import { createReadStream, createWriteStream } from 'node:fs'
 import { readCache, allOf, byExchange, byTickers, byInterval, readLastCachedJsonLineOf } from '../lib/cache.mjs'
-import { isoDateReplacer } from '../lib/date.mjs'
 import { availableTickers } from '../openapi.mjs'
 import { writeCsv, writeJson } from '../lib/jsonl.mjs'
 
-console.log('[bin/sync-coinbase] OpenAPI.availableTickers:', availableTickers)
-console.log('[bin/sync-coinbase] Date.toJSON:', new Date().toJSON())
-console.log('[bin/sync-coinbase] Date.getTimezoneOffset:', new Date().getTimezoneOffset())
+console.log('[bin/build-coinbase-shallow] OpenAPI.availableTickers:', availableTickers)
+console.log('[bin/build-coinbase-shallow] Date.toJSON:', new Date().toJSON())
+console.log('[bin/build-coinbase-shallow] Date.getTimezoneOffset:', new Date().getTimezoneOffset())
 
-//   console.log('[bin/sync-coinbase] Loading: %s', file)
-//   const [lastCachedCandleDate] = await readLastCachedJsonLineOf(createReadStream(file), jsonDateReviver)
-//   console.info('↳ lastCachedCandleDate:', toDateString(lastCachedCandleDate))
-
-for (const interval of ['1d'/*, '1y', '1m'/*, '1w' */]) {
+for (const interval of ['1y', '1m'/*, '1w' */, '1d']) {
+  console.time(`[bin/build-coinbase-shallow] Write: www/api/${interval}.csv`)
   const latestCsvWriter = writeCsv(['ticker', 'date', 'open', 'high', 'low', 'close', 'volume'])
+
+  console.time(`[bin/build-coinbase-shallow] Write: www/api/${interval}.json`)
   const latestJsonWriter = writeJson()
 
-  for (
-    const file of await readCache(
-      allOf(
-        byExchange('coinbase'),
-        byTickers(availableTickers),
-        byInterval(interval)
-      )
-    )
-  ) {
+  for (const file of await readCache(allOf(byExchange('coinbase'), byTickers(availableTickers), byInterval(interval)))) {
+    console.log('[bin/build-coinbase-shallow] Loading: %s', file)
+
     const [,, id] = file.split(/[/,.]/)
     const lastCachedCandle = await readLastCachedJsonLineOf(createReadStream(file))
 
@@ -37,9 +29,9 @@ for (const interval of ['1d'/*, '1y', '1m'/*, '1w' */]) {
   latestJsonWriter.end()
   latestCsvWriter.end()
 
-  console.log(`www/api/${interval}.csv`)
-  latestCsvWriter.pipe(createWriteStream(`www/api/${interval}x.csv`))
+  console.timeEnd(`[bin/build-coinbase-shallow] Write: www/api/${interval}.csv`)
+  latestCsvWriter.pipe(createWriteStream(`www/api/${interval}.csv`))
 
-  console.log(`www/api/${interval}.json`)
-  latestJsonWriter.pipe(createWriteStream(`www/api/${interval}x.json`))
+  console.timeEnd(`[bin/build-coinbase-shallow] Write: www/api/${interval}.json`)
+  latestJsonWriter.pipe(createWriteStream(`www/api/${interval}.json`))
 }
